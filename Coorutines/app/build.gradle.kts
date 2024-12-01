1345818ae93330af193c5633670ebe520899727d
Boldizsarb <donboyszy@gmail.com>
2024-12-01 15:22:24 +0000

    coorutines finished at lifecycles

--- Coorutines/app/build.gradle.kts
+++ Coorutines/app/build.gradle.kts
@@ -49,6 +49,10 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -57,4 +61,7 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
+    implementation("androidx.core:core-ktx:1.15.0") // needed for  the coorutines outside of global
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")// needed for  the coorutines outside of global
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7") // needed for  the coorutines outside of global
 }
--- Coorutines/app/src/main/java/com/example/coorutines/MainActivity.kt
+++ Coorutines/app/src/main/java/com/example/coorutines/MainActivity.kt
@@ -13,8 +13,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.coorutines.ui.theme.CoorutinesTheme
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 
 class MainActivity : ComponentActivity() {
 
@@ -33,11 +37,66 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-        GlobalScope.launch { // global coorutine - global means the coorutine lives until the application does
-            Log.d(TAG,"coorutine says hello from ${Thread.currentThread().name}")
+
+        /*   // Gloabal coorutines
+        GlobalScope.launch (Dispatchers.Main){ // global coorutine - global means the coorutine lives until the application does
+            delay(3000L)  // only pauses the coorutine unlike sleep() /// delays can only called from withing a coorutine
+            Log.d(TAG,"Starting coorutine, hello from ${Thread.currentThread().name}")
         }
         Log.d(TAG," hello from ${Thread.currentThread().name}")
+
+        GlobalScope.launch (Dispatchers.IO){ // IO is used for data operations
+            Log.d(TAG,"Starting coorutine, hello from ${Thread.currentThread().name}")
+            val answer = doNetworkCall()
+            withContext(Dispatchers.Main){ // this switches thread to the chosen thread
+                // this block will be executed in the main thread
+                Log.d(TAG,"Setting text in  ${Thread.currentThread().name}")
+            }
+        }
+        ////////////////////////  Blocking the main thread!
+
+        Log.d(TAG,"Before runBlock")
+        runBlocking { // it blocks the main thread
+            launch (Dispatchers.IO){ // can launch another coorutine from within,
+                // the io thread is not blocked by the runblocking
+                delay(3000L)
+                Log.d(TAG,"finished IO")
+            }
+            Log.d(TAG,"Start of the runBlocking")
+            delay(6000L)
+            Log.d(TAG,"End of runBlocking")
+        }
+        Log.d(TAG,"After runBlocking")
+
+         */
+
+        /*    // run blocking
+        val job = GlobalScope.launch(Dispatchers.Default) { // every launch function returns a job
+            repeat(5){
+                Log.d(TAG,"=Coorutine is still working ....")
+                delay(1000L)
+            }
+        }
+
+        runBlocking {
+            delay(2000L)
+            job.cancel() // cancels the job after 2s
+            //job.join() // waiting for the return of the job // join can only be called from coorutine scope
+            // the join function will block all thread till the job is finished
+            Log.d(TAG,"Main thread is continuing....")
+
+        }
+
+         */
+     //////  new activity
+
+
     }
+}
+
+suspend fun doNetworkCall(): String { // delaying function
+    delay(3000L)
+    return "This is the answer"
 }
 
 @Composable
